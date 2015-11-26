63c729c6ab079d1f15b95a83420eb84b47967d2c
guneetsinghmehta <mehta_guneet@iitj.ac.in>
2015-11-25 18:51:44 -0600

    version2 finalized architecture

---
+++ S1v2.java
@@ -0,0 +1,13 @@
+import java.io.*;//for input and output
+import java.net.*;// for sockets library
+import java.nio.ByteBuffer;
+import java.lang.*;
+import java.util.*;
+import java.lang.instrument.Instrumentation;
+
+public class S1v2 {
+	public static void main(String args[]) throws IOException
+	{
+		Functionsv2.makeTextFile(Datav2.FILENAME);
+	}
+}
